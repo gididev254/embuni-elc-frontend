e70f67a078976b74f39cb96d86ada83a1b2eddef
earn75agencies <gedionmutua2024@gmail.com>
2025-12-05 22:58:17 +0300

    modified 3 files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/*.css"
+  ],
+  safelist: [
+    'sm:px-6' // Safelist the problematic class
   ],
   theme: {
     extend: {
